ad87ec24c3d630f6c82cf407a8fdc8ba2225b282
Jewel Chiong <jewel@Jewels-MacBook-Pro.local>
2024-05-04 21:20:02 -0400

    lesson26:lifting state up

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,33 +1,18 @@
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 
-function NewPost() {
-  // stores a new value in memory but also rexecutes the function in which the state was registered
-  // array destructuring
-  // [current value, updater for currentvalue] = useState(initial value);
-  const [enteredBody, setEnteredBody] = useState("");
-
-  // we automatically get the event object as an argument to the event handler
-  function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setEnteredBody(event.target.value);
-  }
-
-  // calling () after the function name will execute the function, so just use the function name to pass it to the event listener
-  // functions are first-class citizens in JavaScript, so you can pass them around like any other value
-  // In functional programming languages, functions are also first-class citizens. This means you can do things
-  // like assign a function to a variable, pass a function as an argument to another function,
-  // or have a function return another function.
-
+//you could pass the props as an object or define each individual props surrounded by {}
+// ex... props could be {onBodyChange} or {props.onBodyChange}
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,13 +1,43 @@
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 import NewPost from "./NewPost";
+import { useState } from "react";
+
+// ---- LIFTING STATE UP-----
+//if you have state that's manipulated in component A, but needed in component B, you should
+//lift the state up to a component that has access to both compoents that need the state
+// for example, NewPost and Post need the same state, so we lift the state up to PostsList
+// and pass the state and the event handlers to the child components
 
 export function PostsLists() {
+  // stores a new value in memory but also rexecutes the function in which the state was registered
+  // array destructuring
+  // [current value, updater for currentvalue] = useState(initial value);
+  const [enteredBody, setEnteredBody] = useState("");
+
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  // we automatically get the event object as an argument to the event handler
+  function bodyChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  // calling () after the function name will execute the function, so just use the function name to pass it to the event listener
+  // functions are first-class citizens in JavaScript, so you can pass them around like any other value
+  // In functional programming languages, functions are also first-class citizens. This means you can do things
+  // like assign a function to a variable, pass a function as an argument to another function,
+  // or have a function return another function.
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={styles.posts}>
-        <Post author="Jewel" body="Hello, world!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nick" body="Goodbye, world!" />
       </ul>
     </>
