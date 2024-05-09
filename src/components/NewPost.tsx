3a7b9e34403be3c20c0f9a8450de35567807746d
Jewel7 <jewel@Jewels-MacBook-Pro.local>
2024-05-09 16:45:05 -0400

    lesson 31: handling form submission

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -3,18 +3,41 @@ import { useState } from "react";
 
 //you could pass the props as an object or define each individual props surrounded by {}
 // ex... props could be {onBodyChange} or {props.onBodyChange}
-function NewPost({ onBodyChange, onAuthorChange, onCancel }) {
+function NewPost({ onCancel }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event: React.FormEvent) {
+    // prevent the default browser behavior of submitting the form, because we don't have a server to send the data to
+    event.preventDefault();
+    // collect author and body data
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor,
+    };
+
+    console.log(postData);
+    onCancel();
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
-      <p className={classes.action}>
+      <p className={classes.actions}>
         {/* buttons added to a form will submit the form, by default  */}
         {/* to ensure that the cancel button doesn't trigger a form submission, give it a type of button */}
         <button type="button" onClick={onCancel}>
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -4,58 +4,16 @@ import NewPost from "./NewPost";
 import { useState } from "react";
 import Modal from "./Modal";
 
-// ---- LIFTING STATE UP-----
-//if you have state that's manipulated in component A, but needed in component B, you should
-//lift the state up to a component that has access to both compoents that need the state
-// for example, NewPost and Post need the same state, so we lift the state up to PostsList
-// and pass the state and the event handlers to the child components
-
 export function PostsLists({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // ---ALTERNATIVE TO CONDITIONAL RENDERING BELOW---
-  // APPROACH 1: you can store JSX code in variables as well.
-
-  //   let modalContent;
-
-  //   if (modalIsVisible) {
-  //     modalContent = (
-  //       <Modal onClose={hideModalHandler}>
-  //         <NewPost
-  //           onBodyChange={bodyChangeHandler}
-  //           onAuthorChange={authorChangeHandler}
-  //         />
-  //       </Modal>
-  //     );
-  //   }
-
-  // APPROACH 2: ternary expression
-  // APPROACH 3: short-circuiting, where the second operand is not evaluated if the first operand is sufficient
-  // to determine the result. If the modal is truthy, then the component is rendered
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
 
       <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nick" body="Goodbye, world!" />
       </ul>
     </>
