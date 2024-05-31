86692174a0cf96d49324dbe4fa48896148f1dc1f
Jewel <goldameirchiong@gmail.com>
2024-05-31 17:26:42 -0400

    lesson 41: working with layout routes

---
+++ src/routes/RootLayout.tsx
@@ -0,0 +1,16 @@
+import MainHeader from "../components/MainHeader";
+import { Outlet } from "react-router-dom";
+
+// Outlet is a placeholder for the child routes. It renders the nested
+//route content (child routes)
+function RootLayout() {
+  return (
+    <>
+      <MainHeader />
+
+      <Outlet />
+    </>
+  );
+}
+
+export default RootLayout;
